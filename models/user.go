c3c8853f48c3ab3022856ebc84168279055466a1
marioax <santiagoadris171@gmail.com>
2023-09-22 18:49:05 -0300

    adding birthday field to  model

--- models/user.go
+++ models/user.go
@@ -4,6 +4,7 @@ package models
 type UserInfo struct {
 	Email string `json:"email"`
 	FullName string `json:"fullname"`
+	Birthdate string `json:"birthdate"`
 	Nick string `json:"nickname"`
 	Interests []string `json:"interests"`
 	Zone string `json:"zone"`
@@ -13,6 +14,7 @@ type User struct {
 	ID string `json:"uid" gorm:"primary_key"`
 	Email string `json:"email,omitempty"`
 	FullName string `json:"fullname,omitempty"`
+	Birthdate string `json:"birthdate,omitempty"`
 	Nick string `json:"nickname"`
 	Interests []string `json:"interests"`
 	Followers uint `json:"followers,omitempty"`
