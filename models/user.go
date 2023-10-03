4c522f250c6a7efa97d64d850922167580d2dbb7
marioax <santiagoadris171@gmail.com>
2023-10-03 01:23:39 -0300

    adding examples for zone field

--- models/user.go
+++ models/user.go
@@ -4,26 +4,26 @@ type User struct {
 	ID        string             `json:"uid" gorm:"primary_key"`
 	Email     string             `json:"email,omitempty"`
 	FullName  string             `json:"fullname,omitempty"`
-	Birthdate string             `json:"birthdate,omitempty"`
+	Birthdate string             `json:"birthdate,omitempty" example:"YYYY-MM-DD"`
 	Nick      string             `json:"nick"`
 	Follows   int				 `json:"follows"`
 	Followers int				 `json:"followers"`
 	Picture   string             `json:"pic"`
 	Interests []string           `json:"interests"`
 	Ocupation string             `json:"ocupation,omitempty"`
-	Zone      map[string]float32 `json:"zone,omitempty" example:{latitude: 0.00, longitude: 0.00}`
+	Zone      map[string]float32 `json:"zone,omitempty" example:"latitude:0.00,longitude:0.00"`
 	IsAdmin bool `json:"is_admin,omitempty"`
 }
 
 type UserCreate struct {
 	Email     string             `json:"email"`
 	FullName  string             `json:"fullname"`
-	Birthdate string             `json:"birthdate"`
+	Birthdate string             `json:"birthdate" example:"YYYY-MM-DD"`
 	Nick      string             `json:"nick"`
 	Picture   string             `json:"pic"`
 	Interests []string           `json:"interests"`
 	Ocupation string             `json:"ocupation,omitempty"`
-	Zone      map[string]float32 `json:"zone" example:{latitude: 0.00, longitude: 0.00}`
+	Zone      map[string]float32 `json:"zone,omitempty" example:"latitude:0.00,longitude:0.00"`
 }
 
 type UserPublic struct {
