5b9ace100ff51fe4273e0f444de6cced861ed88f
marioax <santiagoadris171@gmail.com>
2023-10-30 19:21:22 -0300

    fix: fixing some typos

--- controllers/posts/posts.go
+++ controllers/posts/posts.go
@@ -114,7 +114,7 @@ func Update(c *gin.Context) {
 	client := &http.Client{};
 	res, err := client.Do(req);
 
-	if err != nil {
+	if (err != nil) {
 		c.JSON(res.StatusCode, gin.H{"error": err.Error()})
 		return
 	}
@@ -138,7 +138,7 @@ func Delete(c *gin.Context) {
 	client := &http.Client{}
 	res, err := client.Do(req)
 
-	if err != nil {
+	if (err != nil) {
 		c.JSON(res.StatusCode, gin.H{"error": err.Error()})
 		return
 	}
@@ -159,31 +159,32 @@ func Delete(c *gin.Context) {
 // @Security Bearer
 func GetFeed(c *gin.Context) {
     uid := c.MustGet("FIREBASE_UID").(string)
-    path_query := strings.Split(c.Request.URL.RequestURI(), "?")[1];
+    query := strings.Split(c.Request.URL.RequestURI(), "?")[1];
 
     // fetch follows list
-    url := fmt.Sprintf("%s/users/%s/follows", USER_URL, uid);
+    url := fmt.Sprintf("%s/users/%s/follows", USERS_URL, uid);
     res, err := http.Get(url);
 
     if (err != nil) {
         c.JSON(res.StatusCode, gin.H{ "error" : err.Error });
     }
     var follows []models.UserPublic;
-    var follows_uid [len(follows)]string;
-    json.NewDecoder(res.Body).Decode(&follows);
-
-    // parse follows uid to http query format
-    var follows_query string;
-
-    for (i, follow := range follows) {
-        follows_query + "uid=" + follow + "&";
+    err = json.NewDecoder(res.Body).Decode(&follows);
+    
+    if (err != nil) {
+        c.JSON(http.StatusInternalServerError, gin.H{ "error" : "cannot parse body" });
+        return;
     }
-    fmt.Printf("[FOLLOWS_UID] %s", follows_query);
+
+     // parse follows uid to http query format
+    for _, follow := range follows {
+        query += "&uid=" + follow.ID;
+    }
 
     // fetch (private and public) posts of followed
-    url := fmt.Sprintf("%s/posts?%s&%s&private=True&public=True", POSTS_URL, uid, path_query, follows_query);
-    fmt.Printf("%s\n", url)
-    res, err := http.Get(url);
+    url = fmt.Sprintf("%s/posts?%s&private=True&public=True", POSTS_URL, query);
+    fmt.Printf("[URL] %s\n", url)
+    res, err = http.Get(url);
 
     if (err != nil) {
         c.JSON(res.StatusCode, gin.H{ "error" : err.Error });
