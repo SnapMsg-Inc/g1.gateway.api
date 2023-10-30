38c1c9794d542405cf6d2949f6b1246e2cac7404
marioax <82295604+marioax@users.noreply.github.com>
2023-10-30 02:16:35 -0300

    wiring post microservice (#8)
    * adding some endpoints?
    
    * add posts functionality
    
    * features completed
    
    ---------
    
    Co-authored-by: agustin <azuretti@fi.uba.ar>

--- models/post.go
+++ models/post.go
@@ -2,20 +2,24 @@ package models
 
 import "time"
 
-type PostInfo struct {
-	UID      string   `json:"-" gorm:"primary_key"`
-	Hashtags []string `json:"hashtags"`
-	Text     string   `json:"text" gorm:"size:300"`
-	MediaURI []string `json:"mediaURI"`
-	Public   bool     `json:"ispublic"`
-}
-
 type Post struct {
 	ID        uint      `json:"pid" gorm:"primary_key"`
-	Nickname  string    `json:"nick"`
+	UID       string    `json:"uid"`
+	Nick  string        `json:"nick"`
 	Timestamp time.Time `json:"timestamp"`
 	Hashtags  []string  `json:"hashtags"`
 	Text      string    `json:"text" gorm:"size:300"`
-	MediaURI  []string  `json:"mediaURI"`
+	MediaURI  []string  `json:"media_uri"`
 	Likes     uint      `json:"likes"`
 }
+
+type PostCreate struct {
+	UID      string   `json:"uid" swaggerignore:"true"`
+	Nick     string   `json:"nick" swaggerignore:"true"`
+	Text     string   `json:"text" gorm:"size:300"`
+	Hashtags []string `json:"hashtags,omitempty"`
+	MediaURI []string `json:"media_uri,omitempty"`
+	Private bool      `json:"is_private"`
+}
+
+
